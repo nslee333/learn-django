ab0e292f4a4a5ce92f7d2a7952da1d33ee673bcc
Nathan <nslee333@gmail.com>
2023-09-30 12:16:41 -0700

    error

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("pools/", include("polls.urls"))
+    path("pools/", include("polls.urls")),
     path('admin/', admin.site.urls),
 ]
